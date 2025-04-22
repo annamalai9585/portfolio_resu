a555284a5ea9b6f9b776ca04512ed850136370e7
mvigneshPrasath <annamammnbj>
2025-04-22 13:19:32 +0530

    File portfolios

---
+++ script.js
@@ -0,0 +1,48 @@
+
+document.getElementById("butt1").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/validations.git';
+   };
+  document.getElementById("butt1v").onclick = function () {
+    location.href ='https://annamalai9585.github.io/validations/';
+  };
+  
+document.getElementById("butt2").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/project-Ayna.git';
+   };
+  document.getElementById("butt2v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/project-Ayna/';
+  };
+
+  document.getElementById("butt3").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Loading.git';
+   };
+  document.getElementById("butt3v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Loading/';
+  };
+  document.getElementById("butt5").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/clock.git';
+   };
+  document.getElementById("butt5v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/clock/';
+  };
+
+  document.getElementById("butt6").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/calculator.git';
+   };
+  document.getElementById("butt6v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/calculator/';
+  };
+
+  document.getElementById("butt4").onclick = function () {
+    location.href = 'https://github.com/annamalai9585/Todo.git';
+   };
+  document.getElementById("butt4v").onclick = function () {
+    location.href = 'https://annamalai9585.github.io/Todo/';
+  };
+  
+  
+
+
+
+
+  
